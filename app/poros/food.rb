0501d8171e87bdbf63153afe0524dbe546522d76
Chris Allbritton <cla.inbox@gmail.com>
2020-10-19 11:51:19 -0400

    Update poros to set attributes to correct data from API call

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -1,8 +1,9 @@
 class Food
-attr_reader :code, :description, :brand, :ingredients
+attr_reader :id, :code, :description, :brand, :ingredients
 
   def initialize(attr)
-    @code = attr[:gtinUPC]
+    @id = attr[:fdcId]
+    @code = attr[:gtinUpc]
     @description = attr[:description]
     @brand = attr[:brandOwner]
     @ingredients = attr[:ingredients]
